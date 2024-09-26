bfba557ed83a6ca3b91cd6e33a82cf60d3e27c83
jonathanjrb <jonathanrodriguesbarbosa@souunisuam.com.br>
2024-09-26 18:32:01 +0100

    corrigindo erros

--- pythonexercicios/ex004.py
+++ pythonexercicios/ex004.py
@@ -1,12 +1,15 @@
 # Só mostra verdadeiro se for numero
 
-n = input('Digite algo 1: ')
-print(n.isnumeric())
+n = input('Digite algo: ')
+print('O tipo primitivo desse valor é ', type(n))
+print('Só tem numero? ', n.isnumeric())
 # Só mostra verdade para alfabeto
 
-o = input('Digite algo 2: ')
-print(o.isalpha())
+o = input('Digite algo: ')
+print('O tipo primitivo desse valor é ', type(o))
+print('Só tem letra? ', o.isalpha())
 # Só mostra verdadeiro se for letra maiuscula
 
 b = input('Digite algo: ')
-print(b.isupper())
+print('O tipo primitivo desse valor é ', type(b))
+print('O formato tá maiusculo? ', b.isupper())

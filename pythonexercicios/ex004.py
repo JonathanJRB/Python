b91d9a961ad2bacdffbe8ba0aaa76457eda0a56d
jonathanjrb <jonathanrodriguesbarbosa@souunisuam.com.br>
2024-09-26 18:19:38 +0100

    ao infinito

---
+++ pythonexercicios/ex004.py
@@ -0,0 +1,12 @@
+# Só mostra verdadeiro se for numero
+
+n = input('Digite algo 1: ')
+print(n.isnumeric())
+# Só mostra verdade para alfabeto
+
+o = input('Digite algo 2: ')
+print(o.isalpha())
+# Só mostra verdadeiro se for letra maiuscula
+
+b = input('Digite algo: ')
+print(b.isupper())

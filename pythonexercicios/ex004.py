9c2527dd74a133d0b5c582554155f918dd9c75ea
jonathanjrb <jonathanrodriguesbarbosa@souunisuam.com.br>
2024-10-05 12:16:47 +0100

    Novas pastas

--- pythonexercicios/ex004.py
+++ pythonexercicios/ex004.py
@@ -3,11 +3,13 @@
 n = input('Digite algo: ')
 print('O tipo primitivo desse valor é ', type(n))
 print('Só tem numero? ', n.isnumeric())
+
 # Só mostra verdade para alfabeto
 
 o = input('Digite algo: ')
 print('O tipo primitivo desse valor é ', type(o))
 print('Só tem letra? ', o.isalpha())
+
 # Só mostra verdadeiro se for letra maiuscula
 
 b = input('Digite algo: ')
